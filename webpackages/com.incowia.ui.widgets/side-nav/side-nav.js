04c05737db5db8e7d573a114f9a28f68dc68f3a7
Philipp Wagner <philipp.wagner@incowia.com>
2016-12-06 12:53:30 +0100

    add side-nav implementation

--- webpackages/com.incowia.ui.widgets/side-nav/side-nav.js
+++ webpackages/com.incowia.ui.widgets/side-nav/side-nav.js
@@ -2,6 +2,25 @@
   'use strict';
 
   CubxPolymer({
-    is: 'side-nav'
+    is: 'side-nav',
+
+    properties: {
+      visibilityClass: {
+        type: String,
+        value: 'hidden'
+      }
+    },
+
+    closeNav: function () {
+      this.set('visibilityClass', 'hidden');
+    },
+
+    openNav: function () {
+      this.set('visibilityClass', '');
+    },
+
+    modelAnchorChanged: function (newValue) {
+      if (newValue !== 'left' && newValue !== 'right') { this.set('model.anchor', 'left'); }
+    }
   });
 }());
